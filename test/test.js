1c2c6394b9a9be119f0c7c5ac7569d3313cda301
root <joshua.orozco@kingoenergy.com>
2018-10-16 16:30:44 -0600

    Changed

--- test/test.js
+++ test/test.js
@@ -1,9 +1,14 @@
 var request = require('supertest');
 var app = require('../index.js');
 
-describe('GET /', function () {
-    it('respond with hello world', function (done) {
-        //navigate to root and check the the response is "hello world"
-        request(app).get('/').expect('{\n  "sucess": "hello world"\n}', done);
+var host_url = 'http://localhost:80'
+var container_url = host_url + '/test';
+
+describe('4 Container Retrieval', function () {
+    container = request(container_url);
+    it('MUST support GET, HEAD, and OPTIONs methods', function (done) {
+        container
+            .get('/')
+            .expect(200, done);
     });
 });
