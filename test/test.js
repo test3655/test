120959e8b37cba379b4e8fbca7403115b60ed139
root <joshua.orozco@kingoenergy.com>
2018-10-16 16:54:39 -0600

    Changed

--- test/test.js
+++ test/test.js
@@ -1,8 +1,8 @@
 var request = require('supertest');
 var app = require('../index.js');
-const config     = require('./config');
+const config = require('../config');
 
-var host_url = 'http://'+config.host_name+':'+config.port;
+var host_url = 'http://' + config.host_name + ':' + config.port;
 var container_url = host_url + '/test';
 
 describe('4 Container Retrieval', function () {
