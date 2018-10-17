585217155474d2f04127687b38f777d9b9877faa
root <joshua.orozco@kingoenergy.com>
2018-10-17 09:42:49 -0600

    Changed on based

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-ssh root@172.16.198.137 <<EOF
-    cd /home/test
-    git pull origin master
-    npm install
-    systemctl restart test
-EOF
+cd /home/test
+git pull origin master
+npm install
+systemctl restart test
